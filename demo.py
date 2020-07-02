8133cc5be4446561d563ec1cbdb3453196364ebe
Zachzw <67519730+Zachzw@users.noreply.github.com>
2020-07-02 17:17:54 -0600

    Update

---
+++ demo.py
@@ -0,0 +1,16 @@
+# def end():
+#     print ('Press Enter key to close window')
+#     input()
+
+
+
+
+# print('Enter your name')
+# name_input = input()
+# print(f'Your name is {name_input}')
+
+
+# end()
+
+Beorn=Making 
+print(len(Beorn))
