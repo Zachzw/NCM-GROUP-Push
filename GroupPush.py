99d13e01d61a4caaacbdea8deda8d8389883b305
Zachzw <67519730+Zachzw@users.noreply.github.com>
2020-07-02 17:15:35 -0600

    push

--- GroupPush.py
+++ GroupPush.py
@@ -2,11 +2,14 @@ import requests
 import csv
 import json
 import logging
+import Group_ID_Prompt
 
-#Import API keys from CSV and assigns them variables
 
-end = 'Press Enter to exit'
+def end_prompt():
+  print('Press Enter to exit')
+  input()
 
+# Import API keys from CSV and assigns them variables
 def main():
 
   with open('API_keys.csv', 'r') as f:
@@ -15,7 +18,7 @@ def main():
 
   a, b, c, d = API_list
 
-#JSON Headers for NCM
+ # JSON Headers for NCM
   headers = {
       'X-CP-API-ID': f'{a[0]}',
       'X-CP-API-KEY': f'{b[0]}',
@@ -23,48 +26,47 @@ def main():
       'X-ECM-API-KEY': f'{d[0]}',
       'Content-Type': 'application/json' 
   }
-  # Pulls Group ID from Group_ID.txt, injects it into the NCM Group endpoint, then assigns that endpoint to a variable
+ # Pulls Group ID from Group_ID.txt, injects it into the NCM Group endpoint, then assigns that endpoint to a variable
 
   with open('Group_ID.txt', 'r') as f:
     group_ID=f.read()    
 
-  print('Your group ID is ' + group_ID)
+  print(f'You entered the group ID: {group_ID}')
   NCMendpoint = f'https://www.cradlepointecm.com/api/v2/groups/{group_ID}/' 
-  print(NCMendpoint)
+  print(f'Your API endpoint is {NCMendpoint}')
 
-  # Imports the payload from Load.txt and verifys it's in a valid JSON format
+ # Imports the payload from Load.txt and verifys it's in a valid JSON format
 
   try:
     with open('Load.txt', 'r') as f:
       payload=f.read()
       payload_json = json.loads(payload) 
   except: 
-    print(f'Invalid JSON, {end}')     
-    input()     
+    print('Invalid JSON file')     
+    end_prompt()    
   else:
-    print('Valid JSON')        
-        
-    # Pulls payload from .txt and assigns it a variable.
+    print('Valid JSON file')        
+             
+ # API call to NCM
 
   r = requests.put(NCMendpoint,json=payload_json,headers =headers)
   r.status_code
  
-  # Prints status codes from the API call
+ # Prints status codes from the API call
  
   if r.status_code == 202:
-    print(f'Status Code: {r.status_code} Successfully PUT config to NCM')
+    print(f'Status Code: {r.status_code}\nSuccessfully PUT config to NCM')
   elif r.status_code == 401:
-    print(f'Status Code: {r.status_code} Authorization Failed: Verify your API keys')
+    print(f'Status Code: {r.status_code}\nAuthorization Failed: Verify your API keys')     
   elif r.status_code == 403:
-    print(f'Status Code: {r.status_code} Verify your Group ID') 
+    print(f'Status Code: {r.status_code}\nVerify your Group ID') 
       
 
-  print(end)
-  input()
+  end_prompt()
 
 
 if __name__ == "__main__":
-    main()
+     main()
 
 
 
